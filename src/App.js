e740f0197a5dea61ca89eb0ea682e4a18fc39c60
Ed Brannin <edbrannin@gmail.com>
2018-04-21 10:31:07 -0400

    lint & render maze

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import mazeGenerator from 'generate-maze'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import logo from './logo.svg'
+import './App.css'
+
+class Controls extends Component {
+  render () {
+    return <p>Controls</p>
   }
 }
 
-export default App;
+const generateMaze = ({ cols, rows }) => mazeGenerator(cols, rows)
+
+const wall = exists => exists && '2px solid black'
+
+class Maze extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      maze: generateMaze(props),
+    }
+  }
+
+  render () {
+    return <table style={{
+      borderCollapse: 'collapse',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    }}>
+      <tbody>
+        { this.state.maze.map(row => (
+          <tr key={row.y}>
+            { row.map(col => (
+              <td
+                key={col.x}
+                style={{
+                  borderTop: wall(col.top),
+                  borderBottom: wall(col.bottom),
+                  borderLeft: wall(col.left),
+                  borderRight: wall(col.right),
+                  whiteSpace: 'pre',
+                  fontFamily: 'monospace',
+                  textAlign: 'left',
+                  borderCollapse: true,
+                  height: '3em',
+                  width: '3em',
+                }}
+              >
+                &nbsp;
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  }
+}
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Controls />
+        <Maze
+          rows={8}
+          cols={8}
+        />
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    )
+  }
+}
+
+export default App

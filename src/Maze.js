dda648cf30e29f155206268baddf2f1052dc7099
Ed Brannin <edbrannin@gmail.com>
2018-04-21 21:33:13 -0400

    Implement moving through the maze, respecting walls

--- src/Maze.js
+++ src/Maze.js
@@ -6,6 +6,73 @@ const generateMaze = ({ cols, rows }) => mazeGenerator(cols, rows)
 const wall = exists => exists && '2px solid black'
 const isGoal = ({ x, y }, { rows, cols }) => (rows - 1 === y) && (cols - 1 === x)
 
+const delta = (col, row) => ({
+  rows: row,
+  cols: col,
+})
+
+/*
+ * left = 37
+ * up = 38
+ * right = 39
+ * down = 40
+ */
+const keyDirection = ({ keyCode }, walls = {}) => {
+  console.log(`Moving by ${keyCode} with walls`, walls)
+  if (keyCode === 37 && !walls.left) {
+    return delta(-1, 0)
+  } else if (keyCode === 38 && !walls.top) {
+    return delta(0, -1)
+  } else if (keyCode === 39 && !walls.right) {
+    return delta(1, 0)
+  } else if (keyCode === 40 && !walls.bottom) {
+    return delta(0, 1)
+  } else {
+    return null
+  }
+}
+
+const MazeCell = ({
+  cell,
+  solveAward,
+  position,
+  mazeSize,
+}) => {
+  const { x, y } = cell
+  let child = (<span>&nbsp;</span>)
+  let backgroundColor
+  if (isGoal(cell, mazeSize)) {
+    backgroundColor = 'gold'
+    child = (<span>{ solveAward }</span>)
+  } else if (x === position.col && y === position.row) {
+    child = (
+      <div style={{
+        borderRadius: '50%',
+        backgroundColor: 'RebeccaPurple',
+        height: '1.5em',
+        width: '1.5em',
+      }}>&nbsp;</div>
+    )
+  }
+
+  return (
+    <td
+      key={cell.x}
+      style={{
+        borderTop: wall(cell.top),
+        borderBottom: wall(cell.bottom),
+        borderLeft: wall(cell.left),
+        borderRight: wall(cell.right),
+        height: '2em',
+        width: '2em',
+        backgroundColor: backgroundColor,
+      }}
+    >
+      {child}
+    </td>
+  )
+}
+
 class Maze extends Component {
   static getDerivedStateFromProps (nextProps, prevState) {
     if (nextProps.rows === prevState.rows && nextProps.cols === prevState.cols) {
@@ -15,22 +82,54 @@ class Maze extends Component {
       rows: nextProps.rows,
       cols: nextProps.cols,
       maze: generateMaze(nextProps),
+      position: {
+        row: 0,
+        col: 0,
+      },
     }
   }
 
   constructor (props) {
     super(props)
     this.state = {
-      position: {
-        row: 0,
-        col: 0,
-      },
     }
+    this.here = () => {
+      const { maze, position } = this.state
+      const { row, col } = position
+      const rowValues = maze[row]
+      if (rowValues) {
+        return rowValues[col]
+      }
+      console.log('ERROR: Unknown maze position:', [row, col])
+    }
+
     this.onKeyDown = (evt) => {
-      // TODO
+      console.log(`Got keypress: ${evt.keyCode}`)
+      const direction = keyDirection(evt, this.here())
+      if (direction) {
+        console.log(`MOVE:`, direction)
+        const newPosition = {
+          row: this.state.position.row + direction.rows,
+          col: this.state.position.col + direction.cols,
+        }
+        if (isGoal({y: newPosition.row, x: newPosition.col}, this.props)) {
+          this.onSolve()
+        } else {
+          this.setState({
+            position: newPosition,
+          })
+        }
+      }
     }
+
     this.onSolve = () => {
-      this.setState({ maze: generateMaze() })
+      this.setState({
+        maze: generateMaze(this.props),
+        position: {
+          row: 0,
+          col: 0,
+        },
+      })
       this.props.onSolve()
     }
   }
@@ -44,35 +143,40 @@ class Maze extends Component {
         marginTop: '1em',
       }}
       onKeyDown={this.onKeyDown}
+      tabIndex="0"
       >
         <tbody>
           { this.state.maze.map(row => (
-            <tr key={row.y}>
-              { row.map(col => (
-                <td
-                  key={col.x}
-                  style={{
-                    borderTop: wall(col.top),
-                    borderBottom: wall(col.bottom),
-                    borderLeft: wall(col.left),
-                    borderRight: wall(col.right),
-                    height: '3em',
-                    width: '3em',
-                    backgroundColor: isGoal(col, this.props) ? 'gold' : undefined,
-                  }}
-                >
-                  { isGoal(col, this.props) ? this.props.solveAward : <span>&nbsp;</span> }
-                </td>
+            <tr key={row[0].y}>
+              { row.map(cell => (
+                <MazeCell
+                  key={cell.x}
+                  cell={cell}
+                  solveAward={this.props.solveAward}
+                  position={this.state.position}
+                  mazeSize={this.props}
+                />
               ))}
             </tr>
           ))}
         </tbody>
       </table>
-      <button
+      <a
         onClick={this.props.onSolve}
+        style={{
+          display: 'block',
+          margin: '1em',
+          padding: '1em',
+          borderRadius: '1em',
+          backgroundColor: 'green',
+          border: '2px solid darkgreen',
+          width: '25%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
       >
         Solve!
-      </button>
+      </a>
     </div>
   }
 }
